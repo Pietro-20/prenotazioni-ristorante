b45aa8e69b3047f6576b5ee06e189464e05fced6
Pietro-20 <robotti.business20@gmail.com>
2025-12-18 17:42:21 +0100

    Create UserPage.tsx

---
+++ src/components/UserPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserPage() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h2>UserPage</h2>
+      <p>Pagina prenotazione (placeholder).</p>
+    </div>
+  );
+}
