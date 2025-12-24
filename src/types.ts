2fd27632994b6d988fa7a3142668df435898ad3b
Pietro-20 <robotti.business20@gmail.com>
2025-12-24 14:18:41 +0100

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+// src/types.ts
+
+export interface Reservation {
+  id: string;
+  name: string;
+  phone: string;
+  date: string;
+  time: string;
+  people: number;
+}
+
+export type ReservationInput = Omit<Reservation, "id">;
