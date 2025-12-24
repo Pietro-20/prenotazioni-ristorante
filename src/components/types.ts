5f8db504678a3ab575c8bf46cacc2dc3104529b7
Pietro-20 <robotti.business20@gmail.com>
2025-12-24 14:11:25 +0100

    Update types.ts

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,1 +1,13 @@
+// src/components/types.ts
+
+export interface Reservation {
+  id: string;
+  name: string;
+  phone: string;
+  date: string;
+  time: string;
+  people: number;
+}
+
+export type ReservationInput = Omit<Reservation, "id">;
 
