8237a3283b685c0c19f85a95cb6cd67871147631
Pietro-20 <robotti.business20@gmail.com>
2025-12-24 14:59:50 +0100

    Update userReservations.ts

--- src/hooks/userReservations.ts
+++ src/hooks/userReservations.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Reservation, ReservationStatus } from '../types';
+import { Reservation, ReservationStatus } from '../types.ts';
 
 const STORAGE_KEY = 'restaurant_reservations_v3';
 
